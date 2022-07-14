ac12849020662192c3a5de0fdf031acf8e7879eb
adesinaroland <adesinaroland@gmail.com>
2022-07-14 10:06:53 -0400

    Roland

--- src/main/java/com/codedifferently/problem02/Solution.java
+++ src/main/java/com/codedifferently/problem02/Solution.java
@@ -1,7 +1,31 @@
 package com.codedifferently.problem02;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Solution {
     public String howManyLettersDoYouSee(String input){
-        return null;
+
+        String[] wordArray = input.split("");
+
+        Map<String, Integer> wordCount = new TreeMap<>();
+
+        for(String word: wordArray){
+            if(!wordCount.containsKey(word))
+                wordCount.put(word, 1);
+            else {
+                int count = wordCount.get(word);
+                wordCount.put(word, count + 1);
+            }
+        }
+
+        Iterator<Map.Entry<String, Integer>> iterator = wordCount.entrySet().iterator();
+        StringBuilder stringBuilder = new StringBuilder();
+        while(iterator.hasNext()){
+            Map.Entry<String, Integer> entry = iterator.next();
+            stringBuilder.append(entry.getKey()+ ":" + entry.getValue());
+        }
+
+        return stringBuilder.toString();
     }
 }
--- src/main/java/com/codedifferently/problem03/Solution.java
+++ src/main/java/com/codedifferently/problem03/Solution.java
@@ -3,6 +3,7 @@ package com.codedifferently.problem03;
 public class Solution {
 
     public Integer[] stringToNumbers(String[] input){
+
         return null;
     }
 }
